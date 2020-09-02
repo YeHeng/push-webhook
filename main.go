35eb97c54de2594aa5772b70ab8213c8a75e5d6f
yeheng <yeheng@zuzuche.com>
2020-09-02 17:20:24 +0800

    添加入职文件

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"github.com/YeHeng/qy-wexin-webhook/handler"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"os"
+	"path"
 	"time"
 )
 
 func Logger() *logrus.Logger {
+	now := time.Now()
+	logFilePath := ""
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+	if err := os.MkdirAll(logFilePath, 0777); err != nil {
+		fmt.Println(err.Error())
+	}
+	logFileName := now.Format("webhook") + ".log"
+	//日志文件
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	//写入文件
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
 
 	//实例化
 	logger := logrus.New()
 
+	//设置输出
+	logger.Out = src
+
 	//设置日志级别
 	logger.SetLevel(logrus.DebugLevel)
 
+	//设置日志格式
+	logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
 	return logger
 }
 
@@ -46,7 +77,7 @@ func LoggerToFile() gin.HandlerFunc {
 		clientIP := c.ClientIP()
 
 		//日志格式
-		logger.Infof("| %3d | %13v | %15s | %s | %s",
+		logger.Infof("| %3d | %13v | %15s | %s | %s |",
 			statusCode,
 			latencyTime,
 			clientIP,

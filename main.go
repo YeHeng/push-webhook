e720bd67a7b6fc87ec89dc2275587090025a1555
yeheng <yeheng@zuzuche.com>
2020-09-04 02:21:30 +0800

    添加日志

--- main.go
+++ main.go
@@ -42,55 +42,12 @@ func Logger() *logrus.Logger {
 	//设置日志级别
 	logger.SetLevel(logrus.DebugLevel)
 
-	//设置日志格式
-	logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-	})
 	return logger
-}
-
-func LoggerToFile() gin.HandlerFunc {
-	logger := Logger()
-	return func(c *gin.Context) {
-		// 开始时间
-		startTime := time.Now()
-
-		// 处理请求
-		c.Next()
-
-		// 结束时间
-		endTime := time.Now()
-
-		// 执行时间
-		latencyTime := endTime.Sub(startTime)
-
-		// 请求方式
-		reqMethod := c.Request.Method
-
-		// 请求路由
-		reqUri := c.Request.RequestURI
-
-		// 状态码
-		statusCode := c.Writer.Status()
-
-		// 请求IP
-		clientIP := c.ClientIP()
-
-		//日志格式
-		logger.Infof("| %3d | %13v | %15s | %s | %s |",
-			statusCode,
-			latencyTime,
-			clientIP,
-			reqMethod,
-			reqUri,
-		)
-	}
 }
 
 func main() {
 	router := gin.Default()
 	logger := Logger()
-	router.Use(LoggerToFile())
 	router.POST("/alertmanager", handler.AlertManagerHandler(logger))
 
 	err := router.Run()

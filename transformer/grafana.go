d3202998d5d4470414841d3e61e2bf2dd4ba225c
yeheng <hengheng8848@gmail.com>
2021-01-12 20:52:51 +0800

    调整value格式

--- transformer/grafana.go
+++ transformer/grafana.go
@@ -4,16 +4,17 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/YeHeng/qy-wexin-webhook/model"
+	"strings"
 )
 
 func GrafanaToMarkdown(notification model.GrafanaAlert) (newsMessage *model.NewMessage, qyWxUrl string, err error) {
 
 	var buffer bytes.Buffer
-
-	buffer.WriteString(fmt.Sprintf("告警项:【%s】\n", notification.RuleName))
+	qyWxUrl = ""
+	ruleUrl := notification.RuleUrl
 
 	for _, alert := range notification.EvalMatches {
-		buffer.WriteString(fmt.Sprintf("指标：【%s】当前值为：%d\n", alert.Metric, alert.Value))
+		buffer.WriteString(fmt.Sprintf("实例：【%s】当前值为：%f\n", alert.Metric, alert.Value))
 	}
 
 	if len(notification.Tags) > 0 {
@@ -21,8 +22,9 @@ func GrafanaToMarkdown(notification model.GrafanaAlert) (newsMessage *model.NewM
 		if len(tags["qyweixin_key"]) > 0 {
 			qyWxUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + tags["qyweixin_key"]
 		}
-	} else {
-		qyWxUrl = ""
+		if len(tags["base_url"]) > 0 {
+			ruleUrl = strings.ReplaceAll(ruleUrl, "http://localhost:3000", tags["base_url"])
+		}
 	}
 
 	article := &model.Article{
